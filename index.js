b6bd89c1791238e5438c3bafda8a71cae082cd42
Samuel May <sam.may.2412@gmail.com>
2020-07-01 22:35:50 +0100

    Linted and cleaned up files

--- index.js
+++ index.js
@@ -2,42 +2,40 @@ const express = require('express');
 
 const app = express();
 
-let data = {
-}
+const data = {
+};
 
 app.get('/api/', (req, res) => {
-  let result = undefined;
+  let result;
 
   // If we've seen the reg before, send the previous data
   // If we've never seen the reg before, make some data, send it, store it
-  if(data[req.query.reg] === undefined) {
+  if (data[req.query.reg] === undefined) {
     result = generateDetails(req.query.reg);
     data[req.query.reg] = result;
   } else {
     result = data[req.query.reg];
   }
 
-  res.status(200);
-  res.json(result);
-  res.end();
+  res.status(200).json(result);
 });
 
 app.get('*', (req, res) => {
-  res.status(404);
-  res.end();
+  res.status(404).end();
 });
 
 /**
  * Takes a registration plate and creates some fake details for the vehicle
- * @param {*} registrationPlate 
+ * @param {*} registrationPlate
  */
-function generateDetails (registrationPlate) {
-  const tax = Math.round(Math.random()), mot = Math.round(Math.random());
+function generateDetails(registrationPlate) {
+  const tax = Math.round(Math.random()); const
+    mot = Math.round(Math.random());
   return {
-    reg : registrationPlate,
-    taxStatus : tax,
-    motStatus : mot
-  }
+    reg: registrationPlate,
+    taxStatus: tax,
+    motStatus: mot,
+  };
 }
 
 app.listen(8080);
